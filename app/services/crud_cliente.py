d3020351f10c4aa7fcddb9994684a96ac0e09d90
Matias Dittler <dittler_matias@yahoo.com>
2025-08-26 22:08:48 -0300

    Arreglos 5

--- app/services/crud_cliente.py
+++ app/services/crud_cliente.py
@@ -1,13 +1,21 @@
 from typing import List, Optional
 from sqlmodel import Session, select
+from sqlalchemy.orm import selectinload
 from app.models.modelo_cliente import Cliente
 from app.schemas.esquema_cliente import ClienteCreate
 
+
 def get_clientes(db: Session) -> List[Cliente]:
-    return db.exec(select(Cliente)).all()
+    statement = select(Cliente).options(
+        selectinload(Cliente.turnos).selectinload("servicio")
+    )
+    return db.exec(statement).all()
 
 def get_cliente(db: Session, cliente_id: int) -> Optional[Cliente]:
-    return db.get(Cliente, cliente_id)
+    statement = select(Cliente).where(Cliente.id_cliente == cliente_id).options(
+        selectinload(Cliente.turnos).selectinload("servicio")
+    )
+    return db.exec(statement).first()
 
 def create_cliente(db: Session, cliente: ClienteCreate) -> Cliente:
     db_cliente = Cliente.from_orm(cliente)
